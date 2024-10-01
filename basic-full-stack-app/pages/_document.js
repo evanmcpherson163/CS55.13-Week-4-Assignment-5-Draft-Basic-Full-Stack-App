b916e601aea5c11e5fa606ccca51a198a944d691
Evan McPherson <evanmcpherson98@yahoo.com>
2024-09-30 18:55:51 -0700

    final

--- basic-full-stack-app/pages/_document.js
+++ basic-full-stack-app/pages/_document.js
@@ -1,12 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <a href = "/familyguy" className = "btn btn-primary mt-3">Family Guy Characters</a>
-        <a href = "/" className = "btn btn-primary mt-3">Back to Home</a>
+        <Link href = "/familyguy" className = "btn btn-primary mt-3">Family Guy Characters</Link>
+        <Link href = "/" className = "btn btn-primary mt-3">Back to Home</Link>
         <Main />
         <NextScript />
       </body>
